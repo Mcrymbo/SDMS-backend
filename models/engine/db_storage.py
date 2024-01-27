0c63f98311233d16f13689eaf5928637f8a9dc41
Abokiey <sweneysam@gmail.com>
2024-01-27 17:55:22 +0300

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,18 @@
 import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session
 from os import getenv
+from models.base_model import Base
 
 
 base = declarative_base()
 
+
 class db_storage:
+	"""connect mysql to the db"""
+	__engine = None
+	__session = None
+
 	def __init__(self):
 		db_user = getenv('db_user')
 		passwd = getenv('db_passwd')
@@ -15,5 +21,27 @@ class db_storage:
 		db_url = f"mysql+mysqlclient://{db_user}:{passwd}@{host}/{database}"
 
 		self.engine = create_engine(db_url)
-		#self.SessionLocal = sessionmaker(autocommit=False, autoflash=False, bind=self.engine)
 
+	def new(self, obj):
+		""" add new item to database """
+		self.__session.add(obj)
+
+	def save(self):
+		"""save all the changes made in the db"""
+		self.__session.commit()
+
+	def delete(self, obj=None):
+		"""delete from the current db session if obj is not None"""
+		if obj is not None:
+			self.__session.delete(obj)
+
+	def reload(self):
+		"""use a sessionmaker to create a cuurent db session"""
+		self.__session = Base.metadata.create_all(self.__engine)
+		factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+		Session = scoped_session(factory)
+		self.__session = Session()
+
+	def close(self):
+		"""Remove session"""
+		self.__session.close()
