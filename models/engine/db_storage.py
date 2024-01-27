909461c12b5fe50aa9b35180720f4b53200ef7b9
Abokiey <sweneysam@gmail.com>
2024-01-27 17:23:44 +0300

    storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,19 @@
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from os import getenv
 
 
-engine = create
+base = declarative_base()
+
+class db_storage:
+	def __init__(self):
+		db_user = getenv('db_user')
+		passwd = getenv('db_passwd')
+		host = getenv('db_host')
+		database = getenv('db_name')
+		db_url = f"mysql+mysqlclient://{db_user}:{passwd}@{host}/{database}"
+
+		self.engine = create_engine(db_url)
+		#self.SessionLocal = sessionmaker(autocommit=False, autoflash=False, bind=self.engine)
+
