2c3dfc9695e5fc747f357c4a563938f47b659ede
Abokiey <sweneysam@gmail.com>
2024-01-31 13:54:39 +0300

    sql

--- db_dev.sql
+++ db_dev.sql
@@ -2,15 +2,15 @@
 
 CREATE DATABASE IF NOT EXISTS sdms_db;
 USE sdms_db;
-CREATE USER IF NOT EXISTS alphonse@'localhost' IDENTIFIED BY 'admin01';
-CREATE USER IF NOT EXISTS aboki@'localhost' IDENTIFIED BY 'admin02';
-CREATE USER IF NOT EXISTS richard@'localhost' IDENTIFIED BY 'admin03';
+CREATE USER IF NOT EXISTS alphonse@'localhost' IDENTIFIED BY 'sdms_pwd';
+CREATE USER IF NOT EXISTS aboki@'localhost' IDENTIFIED BY 'sdms_pwd';
+CREATE USER IF NOT EXISTS richard@'localhost' IDENTIFIED BY 'sdms_pwd';
 
-GRANT ALL PRIVILEGES ON sdms_db.* TO alphonse@'localhost';
+GRANT ALL PRIVILEGES ON sdms_db.* TO alphonce@'localhost';
 GRANT ALL PRIVILEGES ON sdms_db.* TO richard@'localhost';
 GRANT ALL PRIVILEGES ON sdms_db.* TO aboki@'localhost';
 
-GRANT SELECT ON performance_schema.* TO alphonse@'localhost';
+GRANT SELECT ON performance_schema.* TO alphonce@'localhost';
 GRANT SELECT ON performance_schema.* TO richard@'localhost';
 GRANT SELECT ON performance_schema.* TO aboki@'localhost';
 
