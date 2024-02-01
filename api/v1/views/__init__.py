18f977b5566b4edbc64c7f47af364642e00200e6
mcrymbo <croco2023@gmail.com>
2024-02-01 12:40:02 +0300

    corrected blueprint file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,14 +8,8 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.users import *
-<<<<<<< HEAD
-<<<<<<< HEAD
 from api.v1.views.events import *
 from api.v1.views.teams import *
 from api.v1.views.coaches import *
 from api.v1.views.players import *
 from api.v1.views.login import *
-=======
->>>>>>> parent of af88fed... protected some routes with jwt authentication
-=======
->>>>>>> parent of af88fed... protected some routes with jwt authentication
